372161b7db8f66c9f6c5aca4b6071182207e2983
Giovannix97 <33511946+Giovannix97@users.noreply.github.com>
2019-05-14 11:19:00 +0200

    Added Assets

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public GameObject target;
+    public float speed;    
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject body = this.gameObject;
+        float angle = speed;
+        body.transform.RotateAround(target.transform.position, Vector3.up, angle);
+    }
+}
